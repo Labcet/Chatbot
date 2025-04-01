d953dbd2a62cbba3781a40bec78b0ae8f14fbe29
Zhildeer <ernestochuraflores@gmail.com>
2025-04-01 17:21:04 -0500

    Added ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [
+    {
+      name: "chatbot-api",
+      script: "./src/app.ts",
+      watch: true,
+      max_memory_restart: "1000M",
+      exec_mode: "cluster",
+      instances: "1",
+      //cron
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }
+  ],
+}
